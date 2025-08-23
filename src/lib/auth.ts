b7c7afbe95ade9d26cfad313dcaf538f072621d9
anjanah07 <thanjana7@gmail.com>
2025-08-23 18:15:24 +0530

    add: route for auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,28 @@
-// import NextAuth from "next-auth";
-// import Google from "next-auth/providers/google";
-// import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { db } from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import { db } from "./db";
+import Google from "next-auth/providers/google";
 
-// export const {
-//   handlers: { GET, POST },
-//   auth,
-//   signIn,
-//   signOut,
-// } = NextAuth({
-//   adapter: PrismaAdapter(db),
-//   session: { strategy: "database" },
-//   providers: [
-//     Google({
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     }),
-//   ],
-//   callbacks: {
-//     async session({ session, user }) {
-//       // add user.id to session.user for convenience
-//       // @ts-ignore
-//       if (session.user) session.user.id = user.id;
-//       return session;
-//     },
-//   },
-// });
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "database",
+  },
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) session.user = user;
+      return session;
+    },
+  },
+});
